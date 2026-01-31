eb43ef706181bebaf5bbb73e120cfd2e124e0a61
zakisunset <zakisunset@gmail0.com>
2026-01-31 13:58:45 +0700

    Operators

---
+++ python2.py
@@ -0,0 +1,59 @@
+"""
+#
+#Part : Python Operators
+#Arithmetic Operators
+#
+"""
+
+x = 4 + 5
+y = 10 - 2
+z = 3 * 7
+a = 8 / 2
+b = 10 % 3
+c = 2 ** 3
+
+"""
+#
+#Part : Python Operators
+#Assignment Operators
+#
+"""
+
+x = 5
+x += 3
+x =x = + 3
+
+"""
+#
+#Part : Python Operators
+#Comparison Operators
+#
+"""
+
+a = 5
+b = 10
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
+
+"""
+#
+#Part : Python Operators
+#Logical Operators
+#
+"""
+
+x = True
+y = False
+print(x and y)
+print(x or y)
+print(not x)
+print(x & y)
+print(x | y)
+print(~x)
+a = 5
+b = 10
+print((a < 10) and (b > 5))
