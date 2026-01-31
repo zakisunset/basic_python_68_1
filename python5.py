8d746b2594f803e2c15c1e3c95fc2562014f0fae
zakisunset <zakisunset@gmail0.com>
2026-01-31 14:37:31 +0700

    Loop

---
+++ python5.py
@@ -0,0 +1,23 @@
+"""
+#
+#Part : Loop
+#while Loop
+#
+"""
+i = 1
+while i <= 5:
+    print(i)
+    if i == 3:
+        break
+    i += 1
+"""
+#
+#Part : Loop
+#For Loop
+#
+"""
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+    if fruit == "banana":
+        break
