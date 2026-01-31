5a17d66c5c04c169f1678abf7eb57f81c9101177
zakisunset <zakisunset@gmail0.com>
2026-01-31 13:56:54 +0700

    Operators

---
+++ python1.py
@@ -0,0 +1,42 @@
+"""
+#
+# Part: Python Commet
+#
+"""
+#this is a single line comment in python
+"""
+v = s/t
+ความเร็ว ช ระยะทาง / เวลา
+v = ความเร็ว (m/s)
+s = ระยะทาง (m)
+t = เวลา (s)
+"""
+
+print("Hello, World!") # This will print a greeting message
+
+x = 10
+y = "10"
+print(x, y)
+
+x = str(5)
+y = int("5")
+z = float("5.0")
+print(type(x), type(y), type(z))
+
+"""
+#
+# Part: Varirables Naming
+#
+"""
+myvar = 100
+my_var = 200
+_my_var = 300
+myVar = 400
+MYVAR = 500
+
+#carmelcase
+myVariableName = 900
+#pascalcase
+MyVariableName = 1000
+#snakecase
+my_variacle_name = 1100
