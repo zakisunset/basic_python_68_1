26a2d61be7cf67b5a7f283d145f6808f2d4cf9d9
zakisunset <zakisunset@gmail0.com>
2026-01-31 14:11:52 +0700

    Collaction of Python

---
+++ python3.py
@@ -0,0 +1,45 @@
+"""
+#
+#Part : Collection of Python
+#Lists, Tples, sets, Dictionaries
+#
+"""
+
+"""
+#
+#Lists
+#
+"""
+
+this_list = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(this_list)
+print(this_list[1])
+print(this_list[-1])
+print(type(this_list))
+print(len(this_list))
+
+"""
+#
+#Lists
+#
+"""
+
+print(this_set)
+print(type(this_set))
+print(len(this_set))
+
+"""
+#
+#Lists
+#
+"""
+
+this_dict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+    }
+print(this_dict)
+print(type(this_dict["model"]))
+print(type(this_dict))
+print(len(this_dict))   
